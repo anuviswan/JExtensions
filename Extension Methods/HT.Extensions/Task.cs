127877931c626964c9c2071b1a1cb3d846a3fe7b
Anu Viswan <anu@ecsolvo.com>
2018-03-31 17:01:12 +0530

    Fixed Duplicate value issue

--- Extension Methods/HT.Extensions/Task.cs
+++ Extension Methods/HT.Extensions/Task.cs
@@ -10,13 +10,16 @@ namespace HT.Extensions
     {
         public static int[] WaitForFirstN(this System.Threading.Tasks.Task[] tasks,int numberOfTasks)
         {
+            
             if (numberOfTasks > tasks.Length) throw new ArgumentOutOfRangeException();
 
+            var _taskDictionary = Enumerable.Range(0, tasks.Length).ToDictionary(x => tasks[x]);
             List<int> completedTaskIndices = new List<int>();
+
             while (completedTaskIndices.Count < numberOfTasks)
             {
                 var index = System.Threading.Tasks.Task.WaitAny(tasks);
-                completedTaskIndices.Add(index);
+                completedTaskIndices.Add(_taskDictionary[tasks[index]]);
                 tasks = tasks.Where(x => x.Id != tasks[index].Id).ToArray();
             }
             return completedTaskIndices.ToArray<int>();
