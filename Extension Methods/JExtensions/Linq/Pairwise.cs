f03e31311b15e853b7251370dba654f72f456e21
Anu Viswan <anu.viswan@gmail.com>
2020-08-09 10:21:47 +0530

    Code cleanup Removed MoreLinq dependency

---
+++ Extension Methods/JExtensions/Linq/Pairwise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JExtensions.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        /// <summary>
+        /// Pairwise implementation - currently taken from MoreLinq. Should rewrite this.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="resultSelector"></param>
+        /// <returns></returns>
+        internal static IEnumerable<TResult> Pairwise<TSource,TResult>(this IEnumerable<TSource> source, Func<TSource, TSource, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
+
+            return _(); IEnumerable<TResult> _()
+            {
+                using var e = source.GetEnumerator();
+
+                if (!e.MoveNext())
+                    yield break;
+
+                var previous = e.Current;
+                while (e.MoveNext())
+                {
+                    yield return resultSelector(previous, e.Current);
+                    previous = e.Current;
+                }
+            }
+        }
+    }
+}
