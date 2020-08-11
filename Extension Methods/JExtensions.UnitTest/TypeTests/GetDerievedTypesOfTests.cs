b7bd43cdd9809aea05d3f8aaf3489e6f5976f21c
Anu Viswan <anu.viswan@gmail.com>
2020-08-11 06:02:07 +0530

    Working on GetDerievedTypesOf

---
+++ Extension Methods/JExtensions.UnitTest/TypeTests/GetDerievedTypesOfTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JExtensions;
+
+namespace JExtensions.UnitTest.TypeTests
+{
+    public class GetDerievedTypesOfTests
+    {
+        public void GetDerievedTypesOf_ValidScenario(Type source,IEnumerable<Type> expectedResult)
+        {
+            var result = source.GetDerievedTypesOf(this.GetType().Assembly);
+        }
+    }
+}
+
+namespace JExtensions.UnitTest.TypeTests.MockData
+{
+    public class BaseClass { }
+    public class DerievedA :BaseClass{ }
+    public class DerievedB :BaseClass{ }
+    public class DerievedC :DerievedA{ }
+
+    public class AbstractClass { }
+    public class DerievedD:AbstractClass{ }
+    public class DerievedE : AbstractClass { }
+    public class DerievedF : AbstractClass { }
+
+}
---
+++ Extension Methods/JExtensions/Type/GetDerievedTypesOf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace JExtensions
+{
+    public static partial class TypeExtensions
+    {
+
+        public static IEnumerable<Type> GetDerievedTypesOf(this Type source, Assembly assembly)
+        {
+            return new[] { source };
+        }
+    }
+}
