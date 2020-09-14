75538770d3ca829c1abc64cef390ce38df8b1fa6
Anu Viswan <anu.viswan@gmail.com>
2020-09-14 20:17:17 +0530

    Templating ContainsExactly and associated test methods

---
+++ Extension Methods/JExtensions.UnitTest/Linq/IEnumerableContainsExactlyTests.cs
@@ -0,0 +1,32 @@
+﻿using JExtensions.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JExtensions.UnitTest.Linq
+{
+    public class IEnumerableContainsExactlyTests
+    {
+        [Theory]
+        [MemberData(nameof(ContainsExactly_ValidArguementsTestData))]
+        public void ContainsExactly_ValidArguements<T>(IEnumerable<T> source,int expectedCountOfItems,bool expectedResult)
+        {
+            var result = source.ContainsExactly(expectedCountOfItems);
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> ContainsExactly_ValidArguementsTestData => new[]
+        {
+            new object[]{new object[] {1,2,3,4,5},5,true},
+            new object[]{new object[] {1,2,3,4,5},6,false},
+            new object[]{new object[] {1,2,3,4,5},4,true},
+
+            new object[]{new object[] {'a','b','c','d','e'},5,true},
+            new object[]{new object[] {'a','b','c','d','e'},6,false},
+            new object[]{new object[] {'a','b','c','d','e'},4,false},
+        };
+    }
+}
---
+++ Extension Methods/JExtensions/Linq/ContainsExactly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JExtensions.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static bool ContainsExactly<TSource>(this IEnumerable<TSource> source,int numberOfItems)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
