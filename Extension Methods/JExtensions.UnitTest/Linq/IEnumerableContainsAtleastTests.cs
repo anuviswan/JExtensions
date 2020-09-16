92737289cefc9314108f741a4a3501da495801f8
Anu Viswan <anu.viswan@gmail.com>
2020-09-16 06:52:09 +0530

    Added support for ContainsAtleast API

---
+++ Extension Methods/JExtensions.UnitTest/Linq/IEnumerableContainsAtleastTests.cs
@@ -0,0 +1,46 @@
+﻿using JExtensions.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JExtensions.UnitTest.Linq
+{
+    public class IEnumerableContainsAtleastTests
+    {
+        [Theory]
+        [MemberData(nameof(ContainsAtleast_ValidArguementsTestData))]
+        public void ContainsAtleast_ValidArguements<T>(IEnumerable<T> source, int expectedCountOfItems, bool expectedResult)
+        {
+            var result = source.ContainsAtleast(expectedCountOfItems);
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> ContainsAtleast_ValidArguementsTestData => new[]
+        {
+            new object[]{new object[] {1,2,3,4,5},5,true},
+            new object[]{new object[] {1,2,3,4,5},6,false},
+            new object[]{new object[] {1,2,3,4,5},4,true},
+
+            new object[]{new object[] {'a','b','c','d','e'},5,true},
+            new object[]{new object[] {'a','b','c','d','e'},6,false},
+            new object[]{new object[] {'a','b','c','d','e'},4,true},
+        };
+
+
+        [Theory]
+        [MemberData(nameof(ContainsAtleast_InvalidArguementsTestData))]
+        public void ContainsAtleast_InvalidArguements<T>(IEnumerable<T> source, int expectedCountOfItems, Type expectedException)
+        {
+            Assert.Throws(expectedException, () => source.ContainsAtleast(expectedCountOfItems));
+        }
+
+        public static IEnumerable<object[]> ContainsAtleast_InvalidArguementsTestData => new[]
+        {
+            new object[]{null,5,typeof(ArgumentNullException)},
+            new object[]{new object[] {1,2,3,4,5},-1,typeof(ArgumentOutOfRangeException)},
+        };
+    }
+}
---
+++ Extension Methods/JExtensions/Linq/ContainsAtleast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JExtensions.Linq
+{
+    public static partial class EnumerableExtensions
+    {
+        public static bool ContainsAtleast<TSource>(this IEnumerable<TSource> source, int numberOfItems)
+        {
+            if (source is null) throw new ArgumentNullException();
+            if (numberOfItems < 0) throw new ArgumentOutOfRangeException();
+
+            return source.Count() >= numberOfItems;
+        }
+    }
+}
