3d8be5b09441991835989a73ce72255e833ca2d6
Anu Viswan <anu.viswan@gmail.com>
2020-09-20 06:51:55 +0530

    Removed unused classes Added Product Info Added nuget.config Updated Workflow to include Pack & Publish nuget

--- src/JExtensions.BenchmarkTests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JExtensions.BenchmarkTests
-{
-    public class Class1
-    {
-    }
-}
--- src/JExtensions/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace JExtensions
-{
-    public class Class1
-    {
-    }
-}
